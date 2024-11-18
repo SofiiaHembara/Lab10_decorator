665724417f78c7bc6228ad8b669abaaf702130fe
Sofiia Hembara <hembara.pn@ucu.edu.ua>
2024-11-18 19:55:32 +0200

    Fix some errors

--- src/test/java/org/example/decorator/MockedDocumentTest.java
+++ src/test/java/org/example/decorator/MockedDocumentTest.java
@@ -13,16 +13,6 @@ class MockedDocumentTest {
         String parsedContent = mockedDocument.parse();
         assertEquals(expectedContent, parsedContent, "parse() should return the mocked content.");
     }
-
-    @Test
-    void testGetGcsPath() {
-        String expectedPath = "gs://bucket/mockdoc";
-        MockedDocument mockedDocument = new MockedDocument(expectedPath);
-
-        String gcsPath = mockedDocument.getGcsPath();
-        assertEquals(expectedPath, gcsPath, "getGcsPath() should return the correct GCS path.");
-    }
-
     @Test
     void testToString() {
         MockedDocument mockedDocument = new MockedDocument("gs://bucket/mockdoc");
